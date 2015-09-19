03b9a76aa43948ce13c30056da9335776a5cadbf
Phil Oats <phillip.oats@gmail.com>
2015-09-19 13:10:47 +0100

    Repurpose to import/export to WSplit

--- app/src/main/java/philoats/celerysplit/data/RunFileHelper.java
+++ app/src/main/java/philoats/celerysplit/data/RunFileHelper.java
@@ -9,82 +9,116 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.OutputStreamWriter;
+import java.util.ArrayList;
 
 import philoats.celerysplit.models.SplitSet;
+import rx.Observable;
 
 public class RunFileHelper {
 
-    Context context;
+    public static final String IMPORT_PATH = Environment.getExternalStorageDirectory().toString() + "/CelerySplit/Import";
+    private Context context;
 
     public RunFileHelper(Context context)
     {
         this.context = context;
     }
 
-    public void saveToFile(SplitSet set)
+    public void exportFile(SplitSet set)
     {
-        // TODO check for existing file
         try
         {
-            File myFile = new File(Environment.getExternalStorageDirectory() + "/celerysplit", set.getTitle() + ".txt");
-            //myFile.createNewFile();
-            FileOutputStream fOut = new FileOutputStream(myFile);
-            OutputStreamWriter myOutWriter = new OutputStreamWriter(fOut);
-            myOutWriter.write(set.getTitle() + "," + set.getCount() + "\r\n");
+            File folder = new File(Environment.getExternalStorageDirectory().toString() + "/CelerySplit/Export");
+            if (!folder.exists()) {
+                System.out.println(folder.mkdirs() || folder.isDirectory());
+            }
+
+            File file = new File(folder.getAbsolutePath(), set.getTitle());
+            FileOutputStream fOut = new FileOutputStream(file);
+            OutputStreamWriter outWriter = new OutputStreamWriter(fOut);
+
+            outWriter.write("Title=" + set.getTitle() + "\r\n");
+            outWriter.write("Attempts=" + "0" + "\r\n");
+            outWriter.write("Offset=" + "0" + "\r\n");
+            outWriter.write("Size=120,25" + "\r\n");
             for (int i = 0; i < set.getCount(); i++)
             {
-                myOutWriter.write(set.names[i] + "," + set.pbTimes[i] + "," + set.bestSegments[i] + "\r\n");
+                outWriter.write(set.names[i] + "," + "0," + ((float) set.pbTimes[i]) / 1000 + "," + ((float) set.bestSegments[i]) / 1000 + "\r\n");
             }
-            myOutWriter.close();
+            outWriter.close();
             fOut.close();
+
             Toast.makeText(context,
-                    "Done writing to SD " + set.getTitle() + ".txt'",
+                    file.getAbsolutePath(),
                     Toast.LENGTH_SHORT).show();
         }
         catch (Exception e)
         {
+            System.out.println(e.getMessage());
             Toast.makeText(context, e.getMessage(),
-                    Toast.LENGTH_SHORT).show();
+                    Toast.LENGTH_LONG).show();
         }
     }
 
-    public SplitSet readFromFile(String path)
+    public Observable<SplitSet> importFile(String path)
     {
         String[] names;
         long[] pb;
         long[] best;
         String title;
         String[] curLine;
-        File file = new File(Environment.getExternalStorageDirectory(), path);
+        File file = new File(RunFileHelper.IMPORT_PATH + "/" + path);
 
+        ArrayList<String> splits = new ArrayList<>();
         try
         {
             BufferedReader br = new BufferedReader(new FileReader(file));
-            curLine = br.readLine().split(",");
-            title = curLine[0];
-            int count = Integer.parseInt(curLine[1]);
+            curLine = br.readLine().split("=");
+            title = curLine[1];
+            br.readLine(); br.readLine(); br.readLine();
+
+            while(true)
+            {
+                String next = br.readLine();
+                if (next.startsWith("Icons=")) break;
+                else splits.add(next);
+            }
+
+            int count = splits.size();
             names = new String[count];
             pb = new long[count];
             best = new long[count];
+
             for (int i = 0; i < count; i++)
             {
-                curLine = br.readLine().split(",");
+                curLine = splits.get(i).split(",");
                 names[i] = curLine[0];
-                pb[i] = Long.parseLong(curLine[1]);
-                best[i] = Long.parseLong(curLine[2]);
+                pb[i] = (long) (Float.parseFloat(curLine[2]) * 1000);
+                best[i] = (long) (Float.parseFloat(curLine[3])* 1000);
             }
+
             br.close();
             Toast.makeText(context,
                     "Finished loading " + title + "!",
                     Toast.LENGTH_SHORT).show();
 
-            return new SplitSet(-1, title, names, pb, best);
+            return Observable.just(new SplitSet(-1, title, names, pb, best));
         }
         catch (Exception e)
         {
-           System.out.println("oh shit " + e.getMessage());
+           System.out.println("Error: " + e.getMessage());
         }
 
         return null;
+    }
+
+    public ArrayList<String> getImportFiles() {
+        File dir = new File(IMPORT_PATH);
+        File[] fileList = dir.listFiles();
+        ArrayList<String> files = new ArrayList<>();
+        for (File f : fileList){
+            files.add(f.getName());
+        }
+        return files;
     }
 }
