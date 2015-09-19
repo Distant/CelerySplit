31899f2b769dc86ce7afb4229076ddfe148ce332
Phil Oats <phillip.oats@gmail.com>
2015-09-19 13:18:04 +0100

    Add methods to import/export to WSplit

--- app/src/main/java/philoats/celerysplit/presenters/RunListPresenter.java
+++ app/src/main/java/philoats/celerysplit/presenters/RunListPresenter.java
@@ -1,30 +1,33 @@
 package philoats.celerysplit.presenters;
 import android.content.Context;
+import android.util.Log;
 
 import java.util.ArrayList;
 
+import philoats.celerysplit.data.RunFileHelper;
 import philoats.celerysplit.models.Run;
 import philoats.celerysplit.data.RunDataAccess;
 import philoats.celerysplit.models.SplitSet;
 
+import rx.functions.Action1;
 import rx.Observable;
 import rx.subjects.BehaviorSubject;
 
 public class RunListPresenter implements Presenter, EditRunPresenter.EditListener {
 
     private RunDataAccess dataAccess;
-    private SplitFragmentListener listener;
+    private Action1<SplitSet> onLoadSplits;
 
     private BehaviorSubject<ArrayList<Run>> runSubject;
     public Observable<ArrayList<Run>> runObservable() {
         return runSubject;
     }
 
-    public RunListPresenter(Context context,  RunListPresenter.SplitFragmentListener listener){
+    public RunListPresenter(Context context, Action1<SplitSet> onLoadSplits){
         //this.parent = fragment;
-        this.listener = listener;
+        this.onLoadSplits = onLoadSplits;
         runSubject = BehaviorSubject.create();
-        dataAccess = new RunDataAccess(context); // INJECT
+        dataAccess = new RunDataAccess(context);
     }
 
     public void refresh(){
@@ -32,7 +35,7 @@ public class RunListPresenter implements Presenter, EditRunPresenter.EditListene
     }
 
     public void onDetach(){
-        listener = null;
+        onLoadSplits = null;
     }
 
     @Override
@@ -57,9 +60,9 @@ public class RunListPresenter implements Presenter, EditRunPresenter.EditListene
     }
 
     public void onItemClick(Run run) {
-        if (null != listener) {
+        if (null != onLoadSplits) {
             dataAccess.getSet(run)
-                    .subscribe(listener::onLoadSplits);
+                    .subscribe(onLoadSplits::call);
         }
     }
 
@@ -68,7 +71,20 @@ public class RunListPresenter implements Presenter, EditRunPresenter.EditListene
         dataAccess.getRuns().subscribe(runSubject::onNext);
     }
 
-    public interface SplitFragmentListener {
-        public void onLoadSplits(SplitSet set);
+    public void exportFile(Context context, Run run){
+        RunFileHelper fileHelper = new RunFileHelper(context);
+        dataAccess.getSet((run)).subscribe(fileHelper::exportFile);
+    }
+
+    public void importFile(Context context, String name){
+        RunFileHelper fileHelper = new RunFileHelper(context);
+        fileHelper.importFile(name).subscribe(dataAccess::addRun);
+    }
+
+    public ArrayList<String> getImportFiles(Context context) {
+        RunFileHelper helper = new RunFileHelper(context);
+        ArrayList<String> files;
+        files = helper.getImportFiles();
+        return files;
     }
 }
